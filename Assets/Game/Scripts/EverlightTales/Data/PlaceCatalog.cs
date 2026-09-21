8ea8b10d6a8f14f39dffceb3fbd993a035bd8cfb
唐鑫阳 <azhvo730700@163.com>
2026-09-21 09:44:11 +0800

    完成 b24 G3 一天闭环验收门

--- Assets/Game/Scripts/EverlightTales/Data/PlaceCatalog.cs
+++ Assets/Game/Scripts/EverlightTales/Data/PlaceCatalog.cs
@@ -13,6 +13,7 @@ namespace Everlight.Tales.Data
                 new PlaceConfig("street", "街区小店", "卷帘门／夜班送还", PlaceUnlockSource.Story, 1f, 0f),
                 new PlaceConfig("dance", "舞蹈教室", "红舞鞋调查与维修", PlaceUnlockSource.Story, 0f, 1f),
                 new PlaceConfig("tailor", "裁缝铺", "画皮线索", PlaceUnlockSource.Investigate, 2f, 1f),
+                new PlaceConfig("community", "社区活动中心", "积水处置／邻里找物", PlaceUnlockSource.Story, 2f, 0f),
             };
         }
     }
